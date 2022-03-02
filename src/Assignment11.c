4118c56d6ad2c0e2babe8b4cd2f738db51b45d21
Niklas-Mezynski <n.mezynski@student.fontys.nl>
2022-03-02 20:30:04 +0100

    Assignment11 done

--- src/Assignment11.c
+++ src/Assignment11.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
-#define arr_size 7
+#define arr_size 5
 
 enum week
 {
@@ -15,6 +16,8 @@ enum week
 
 typedef enum week Week;
 
+const char *weekStrings[] = {"Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"};
+
 Week weekArr[arr_size];
 
 int readFromConsole();
@@ -22,10 +25,44 @@ int printToConsole();
 
 int main(int argc, char **argv)
 {
-    Week day;
-    day = Wed;
-    if (day == Wed)
-        printf("%d\n", day);
+    readFromConsole();
+
+    printf("\nThe weekdays you entered were:\n");
+    printToConsole();
     return 0;
 }
 
+
+int readFromConsole()
+{
+
+    for (size_t i = 0; i < arr_size; i++)
+    {
+        printf("Enter a weekday\n");
+        char input[5];
+        scanf("%s", input);
+        short wasFound = 0;
+        for (size_t j = 0; j < 7; j++)
+        {
+            if (strcmp(weekStrings[j], input) == 0)
+            {
+                weekArr[i] = j;
+                wasFound = 1;
+                break;
+            }
+        }
+        if (wasFound == 0) {
+            printf("Invalid day\n");
+            i--;
+        }
+    }
+}
+
+
+int printToConsole()
+{
+    for (size_t i = 0; i < sizeof(weekArr) / sizeof(weekArr[0]); i++)
+    {
+        printf("%d: %s\n", i + 1, weekStrings[weekArr[i]]);
+    }
+}
